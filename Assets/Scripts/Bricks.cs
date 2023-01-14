9d9c93a4ad0c32e9abaf821e21cf4403c79e9949
sbot <sbotejaru2@gmail.com>
2023-01-14 18:55:48 +0200

    Added destroy animation

--- Assets/Scripts/Bricks.cs
+++ Assets/Scripts/Bricks.cs
@@ -2,11 +2,13 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using static UnityEngine.ParticleSystem;
 
 public class Bricks : MonoBehaviour
 {
     public int HitPoints = 1;
     public ParticleSystem DestroyEffect;
+    private SpriteRenderer sr;
 
     public static event Action<Bricks> OnBrickDestruction;
 
@@ -27,10 +29,21 @@ public class Bricks : MonoBehaviour
         }
     }*/
 
+    private void SpawnDestroyEffect()
+    {
+        Vector3 brickPos = gameObject.transform.position;
+        Vector3 spawnPosition = new Vector3(brickPos.x, brickPos.y, brickPos.z - 0.2f);
+        GameObject effect = Instantiate(DestroyEffect.gameObject, spawnPosition, Quaternion.identity);
+
+        MainModule mm = effect.GetComponent<ParticleSystem>().main;
+        mm.startColor = this.sr.color;
+        Destroy(effect, DestroyEffect.main.startLifetime.constant);
+    }
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        this.sr = this.GetComponent<SpriteRenderer>();
     }
 
     // Update is called once per frame
