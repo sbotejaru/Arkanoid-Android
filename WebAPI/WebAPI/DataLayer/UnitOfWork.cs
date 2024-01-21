94531462549254d3032a6d51783bc871e67115da
sbot <sbotejaru2@gmail.com>
2024-01-21 20:20:51 +0200

    Add class for DataLayer connection

---
+++ WebAPI/WebAPI/DataLayer/UnitOfWork.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DataLayer.Repositories;
+
+namespace DataLayer
+{
+    public class UnitOfWork
+    {
+        public ScoreRepository Scores { 
+            get
+            {
+                return new ScoreRepository();
+            }
+        }
+    }
+}
